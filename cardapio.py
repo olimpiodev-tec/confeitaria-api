1545a0bf10915b1e7607494f12df89a2ae44f94e
moises.filho <moises.filho@ifsp.edu.br>
2025-11-18 23:33:08 +0000

    Enviando o projeto

---
+++ cardapio.py
@@ -0,0 +1,12 @@
+
+def buscar_cardapio():
+    itens = [
+        {
+            "id": 1,
+            "nome": "Chocomoça",
+            "descricao": "Bolo de chocolate com ninho",
+            "preco": 49.99,
+            "foto": ""
+        }
+    ]
+    return itens
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
 
 from flask import Flask
+from cardapio import buscar_cardapio
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  """Example Hello World route."""
-  name = os.environ.get("NAME", "World")
-  return f"Hello {name}!"
+def hello_world():  
+  return {
+    "message": "Api rodando"
+  }
+
+@app.route("/cardapio")
+def cardapio():
+  return buscar_cardapio()
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
