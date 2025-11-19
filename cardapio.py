98abba36df2cf1f7685bae1defc3102deedc79f2
moises.filho <moises.filho@ifsp.edu.br>
2025-11-19 01:25:08 +0000

    Conectando no banco de dados

--- cardapio.py
+++ cardapio.py
@@ -1,15 +1,15 @@
+from conexao import get_conexao
+from psycopg2.extras import RealDictCursor
+from flask import jsonify
 
 def buscar_cardapio():
-    itens = [
-        {
-            "id": 1,
-            "nome": "Chocomoça",
-            "descricao": "Bolo de chocolate com ninho",
-            "preco": 49.99,
-            "foto": ""
-        }
-    ]
-    return itens 
+    conn = get_conexao()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM cardapio")
+    cardapio = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(cardapio) 
 
 def buscar_por_id(item_id):
     return {
